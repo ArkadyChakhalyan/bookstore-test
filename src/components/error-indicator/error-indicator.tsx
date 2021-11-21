9888755304c0a868dcbc316ebb259afe013011a3
Arkady Chakhalyan <arkadychakhalyan@gmail.com>
2021-11-21 13:19:38 +0300

    123

--- src/components/error-indicator/error-indicator.tsx
+++ src/components/error-indicator/error-indicator.tsx
@@ -1,10 +1,23 @@
+import { useNavigate } from 'react-router';
 import styles from './error-indicator.module.css';
 
 export const ErrorIndicator = () => {
+
+    const history = useNavigate();
+
+    const onClick = () => {
+        history('/');
+    };
+
     return (
         <div className={styles.error}>
             <p className={styles.title}>Error!</p>
             <p>Something went wrong</p>
+            <button
+                className={styles.button}
+                onClick={onClick} >
+                Go to homepage
+            </button>
         </div>
     );
 };
